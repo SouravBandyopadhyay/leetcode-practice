95b8bd281e25138dadb46f8e75ec883d7f56cef2
Sourav Bandyopadhyay <103631425+SouravBandyopadhyay@users.noreply.github.com>
2026-02-18 12:54:39 +0530

    Time: 12 ms (16.25%) | Memory: 19.8 MB (45.84%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        left , right = 0 , len(s) - 1
+        left, right = 0 , len(s)- 1
 
         def is_alphanumeric(str):
             return str.isalnum()
@@ -14,9 +14,8 @@ class Solution:
             
             if s[left].lower() != s[right].lower():
                 return False
-            
+        
             left +=1
             right -=1
-            
         
         return True
