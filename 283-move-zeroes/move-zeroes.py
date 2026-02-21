5be66d63bdc568c9003d287a453a1ba94487732d
Sourav Bandyopadhyay <103631425+SouravBandyopadhyay@users.noreply.github.com>
2026-02-21 20:23:27 +0530

    Time: 7 ms (42.99%) | Memory: 20.6 MB (20.35%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,8 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left = 0 
-        for right in range(len(nums)):
-            if nums[right]:
-                nums[left],nums[right] = nums[right],nums[left]
-                left +=1
+        temp = []
+        for num in nums:
+            if num !=0:
+                temp.append(num)
+        
+        for i in range(len(nums)):
+            if i <len(temp):
+                nums[i] = temp[i]
+            
+            else:
+                nums[i] = 0
+        
