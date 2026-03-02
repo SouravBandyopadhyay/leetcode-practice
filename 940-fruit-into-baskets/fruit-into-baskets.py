79cd89b095fae831fdbf145d231b4fde4b802918
Sourav Bandyopadhyay <103631425+SouravBandyopadhyay@users.noreply.github.com>
2026-03-02 20:34:30 +0530

    Time: 181 ms (73.53%) | Memory: 25.8 MB (80.01%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,18 +1,18 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         count = defaultdict(int)
-        l = 0
-        max_len = 0
-
+        l = 0 
+        max_len = 0 
         for r in range(len(fruits)):
             count[fruits[r]] += 1
 
-            while len(count) > 2:
-                count[fruits[l]] -= 1
+            while len(count)>2:
+                count[fruits[l]] -=1
                 if count[fruits[l]] == 0:
-                    del count[fruits[l]]
-                l += 1
-
-            max_len = max(max_len, r - l + 1)
-
+                    del count[fruits[l]] 
+                
+                l +=1
+            
+            max_len=max(max_len,r-l+1)
+        
         return max_len
