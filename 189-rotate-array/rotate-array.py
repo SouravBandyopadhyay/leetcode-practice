0d69d7b10da5ef3bc963c7029b05dcb17491274c
Sourav Bandyopadhyay <103631425+SouravBandyopadhyay@users.noreply.github.com>
2026-03-16 11:01:55 +0530

    Time: 8 ms (33.66%) | Memory: 26.4 MB (97.70%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -4,15 +4,15 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-
-        k %= n
+        k %=n
 
         def reverse(l,r):
             while l<r:
-                nums[l],nums[r] = nums[r],nums[l]
+                nums[l],nums[r] = nums[r], nums[l]
                 l+=1
                 r-=1
         
         reverse(0,n-1)
         reverse(0,k-1)
         reverse(k,n-1)
+        
