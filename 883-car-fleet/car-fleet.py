9bc695d750e9f491bb769dc04c8f6591de527e8a
Sourav Bandyopadhyay <103631425+SouravBandyopadhyay@users.noreply.github.com>
2026-04-27 12:25:34 +0530

    Time: 192 ms (35.73%) | Memory: 41.7 MB (45.39%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,12 +1,13 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        cars = sorted(zip(position,speed), key=lambda x:-x[0])
+        cars = sorted(zip(position,speed), key = lambda x:-x[0])
 
         fleets = []
         for p,s in cars:
-            time = (target-p)/ float(s)
+            time = (target-p)/float(s)
 
-            if not fleets or time> fleets[-1]:
+            if not fleets or time > fleets[-1]:
                 fleets.append(time)
+            
         
         return len(fleets)
