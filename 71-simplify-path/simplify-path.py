1d8f78edeacc21adc583f6c0f0e2055cd2691e4f
Sourav Bandyopadhyay <103631425+SouravBandyopadhyay@users.noreply.github.com>
2026-05-08 12:42:36 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (63.56%) - LeetSync

---
+++ 71-simplify-path/simplify-path.py
@@ -0,0 +1,18 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        component = path.split("/")
+        stack = []
+
+        for comp in component:
+            if comp == "" or comp == ".":
+                continue
+            
+            elif comp == "..":
+                if stack:
+                    stack.pop()
+            
+
+            else:
+                stack.append(comp)
+        
+        return '/'+'/'.join(stack)
